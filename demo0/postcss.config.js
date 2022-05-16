34ead7f02d891271c004307eae575db8c322ce53
ronnycyy <m17722548276@163.com>
2022-05-16 23:43:09 +0800

    给demo0加点注释✏️

--- demo0/postcss.config.js
+++ demo0/postcss.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   plugins: {
+    // 支持 css-next
     'postcss-preset-env': {
       stages: 0,
       features: {
+        /**
+         * 启用嵌套规则，如
+         * 
+         * .test {
+         *     & h1 {
+         *       color: yellowgreen;
+         *     }
+         * }
+         */
         'nesting-rules': true,
       },
     },
